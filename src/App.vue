544f7da85678abeb2dc8d0461097524acba11847
gulifan <gulifan@dxy.cn>
2019-04-06 19:28:09 +0800

    chore:viewport和rem设置

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,22 @@
   </div>
 </template>
 
+<script>
+export default {
+  
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth / 10
+  fontSize = fontSize > 50 ? 50 : fontSize
+  html.style.fontSize = fontSize + 'px' 
+})
+</script>
+
 <style scoped>
 .text {
   font-family: 'Days One';
-  font-size: 20px;
+  font-size: 1rem;
   color: orange;
 }
 </style>
