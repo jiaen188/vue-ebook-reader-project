f9d397c59739b5b34b0c9114268117f11b82f17d
gulifan <gulifan@dxy.cn>
2019-04-08 00:03:58 +0800

    feat:翻页功能实现

--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -19,6 +19,19 @@ export default {
     ...mapActions('book', [
       'setFileName'
     ]),
+    prevPage() {
+      if (this.rendition) {
+        this.rendition.prev()
+      }
+    },
+    nextPage() {
+      if (this.rendition) {
+        this.rendition.next()
+      }
+    },
+    toggleTitleAndMenu() {
+
+    },
     initEpub() {
       const url = 'http://192.168.31.243:8081/epub/' + this.fileName + '.epub'
       this.book = new Epub('/2018_Book_AgileProcessesInSoftwareEngine.epub' || url)
@@ -29,6 +42,24 @@ export default {
         methods: 'default'
       })
       this.rendition.display()
+      this.rendition.on('touchstart', event => {
+        this.touchStartX = event.changedTouches[0].clientX
+        this.touchStartTime = event.timeStamp
+      })
+      this.rendition.on('touchend', event => {
+        const offsetX = event.changedTouches[0].clientX - this.touchStartX
+        const time = event.timeStamp - this.touchStartTime
+        console.log('offset, time', offsetX, time)
+        if (time < 500 && offsetX > 40) {
+          this.prevPage()
+        } else if (time < 500 && offsetX < -40) {
+          this.nextPage()
+        } else {
+          this.toggleTitleAndMenu()
+        }
+        event.preventDefault()
+        event.stopPropagation()
+      })
     }
   },
   async mounted() {
