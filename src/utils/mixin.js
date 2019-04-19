1e20b12d5d9edb4876f4033b8ac1c9e77e3bf073
jiaen188 <18258335270@163.com>
2019-04-19 19:43:24 +0800

    feat:目录的背景

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -75,6 +75,11 @@ export const ebookMixin = {
         })
       }
       if (cb) cb()
+    },
+    hideTitleAndMenu() {
+      this.setMenuVisible(false)
+      this.setSettingVisible(-1)
+      this.setFontFamilyVisible(false)
     }
   }
 }
