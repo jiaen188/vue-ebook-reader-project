867bee07c9fc44cdc5f23193abda278c11bbfe6a
jiaen188 <18258335270@163.com>
2019-05-06 14:51:24 +0800

    feat: 书签的下拉效果

--- src/components/ebook/EbookBookmark.vue
+++ src/components/ebook/EbookBookmark.vue
@@ -1,30 +1,65 @@
 <template>
-  <div class="ebook-bookmark">
+  <div class="ebook-bookmark" ref="bookmark">
     <div class="ebook-bookmark-text-wrapper">
-      <div class="ebook-bookmark-down-wrapper">
+      <div class="ebook-bookmark-down-wrapper" ref="iconDown">
         <span class="icon-down"></span>
       </div>
       <div class="ebook-bookmark-text">{{text}}</div>
     </div>
     <div class="ebook-bookmark-icon-wrapper">
-      <bookmark color="red" :width="15" :height="35"></bookmark>
+      <bookmark :color="color" :width="15" :height="35"></bookmark>
     </div>
   </div>
 </template>
 
 <script>
 import Bookmark from '../common/Bookmark'
+import { realPx } from '../../utils/utils'
+import { ebookMixin } from '../../utils/mixin'
 
 const BLUE = '#346cbc'
 const WHITE = '#fff'
 
 export default {
+  mixins: [ebookMixin],
   components: {
     Bookmark
   },
+  computed: {
+    height() {
+      return realPx(35)
+    },
+    threshold() {
+      return realPx(55)
+    }
+  },
+  watch: {
+    offsetY(v) {
+      if (v >= this.height && v <= this.threshold) {
+        // 状态2: 未到达临界状态
+        this.$refs.bookmark.style.top = `${-v}px`
+        this.text = this.$t('book.pulldownAddMark')
+        this.color = WHITE
+        const iconDown = this.$refs.iconDown
+        if (iconDown.style.transform === 'rotate(180deg)') {
+          iconDown.style.transform = 'rotate(0deg)'
+        }
+      } else if (v > this.threshold) {
+        // 状态3: 超过临界状态
+        this.$refs.bookmark.style.top = `${-v}px`
+        this.text = this.$t('book.releaseAddMark')
+        this.color = BLUE
+        const iconDown = this.$refs.iconDown
+        if (iconDown.style.transform === '' || iconDown.style.transform === 'rotate(0deg)') {
+          iconDown.style.transform = 'rotate(180deg)'
+        }
+      }
+    }
+  },
   data() {
     return {
-      text: this.$t('book.pulldownAddMark')
+      text: this.$t('book.pulldownAddMark'),
+      color: WHITE
     }
   }
 }
